3389eee7f9d61c5c996dc14d49715fb06c732193
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-07-07 02:47:51 +0200

    TEST CASE BLOCKBUSTER

---
+++ BlockbusterProject/tests/test_blockbuster.py
@@ -0,0 +1,8 @@
+import unittest
+from unittest import TestCase
+from src.Blockbuster.film import Film
+from src.Blockbuster.movie_genre import Azione, Drama, Commedia
+from src.Blockbuster.noleggio import Noleggio
+
+
+
