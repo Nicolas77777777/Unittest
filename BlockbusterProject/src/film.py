b6afc5c6993774e7f107392edb2a55dc560edaa8
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-07-08 10:34:40 +0200

    fist commit

--- BlockbusterProject/src/film.py
+++ BlockbusterProject/src/film.py
@@ -1,18 +1,3 @@
-"""" n un file chiamato "film.py", si definisca la classe Film che rappresenta un film preso a nolleggio. 
-In tale classe, definire un codice identificativo (int) ed un titolo (string). 
-Entrambi gli attributi sono da considerarsi privati.
- 
-- Definire i seguenti metodi:
-
-    init(id, title): metodo costruttore.
-    setID(id): che consente di impostare il codice identificativo del film, 
-    modificando il valore del relativo attributo.
-    setTitle(title): che consente di impostare il codice identificativo
-    del film, modificando il valore del relativo attributo.
-    getID(): che consente di ritornare il valore del codice identificativo di un film.
-    getTitle(): che consente di ritornare il valore del titolo di un film.
-    isEqual(otherFilm): che ritorna true se il codice identificativo di due film è uguale."""
-
 
 class Film:
     def __init__(self, id:int, title:str ) :
